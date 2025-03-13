8a0fc3eb9c329b48cf598a12b30062618b3e7878
Farah0Jaber <rubymjaber@gmail.com>
2025-03-13 15:47:54 -0700

    login change

--- code/client/src/pages/login.js
+++ code/client/src/pages/login.js
@@ -1,12 +1,59 @@
 import React from "react";
-import { Typography, Paper } from "@mui/material";
+import { Typography, Paper, TextField, Button } from "@mui/material";
 
 function Login() {
     return (
-        <Paper elevation={3} style={{ padding: "20px", textAlign: "center" }}>
-            <Typography variant="h4">Login</Typography>
-            <Typography variant="body1">Welcome to the Login Page.</Typography>
-        </Paper>
+        <div style={{ 
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "#f8fbff", /* Very light blue */
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: 30,
+            padding: 0,
+            overflow: "hidden"
+        }}>
+            <Paper elevation={3} style={{ 
+                padding: "30px", width: "350px", textAlign: "center", borderRadius: "10px",
+                backgroundColor: "white"
+            }}>
+                <img src="/logo.png" alt="Logo" style={{ width: "80px", marginBottom: "15px" }} />
+
+
+                <TextField 
+                    fullWidth 
+                    label="Email" 
+                    variant="outlined" 
+                    margin="normal" 
+                    InputProps={{ style: { borderRadius: "10px" } }}
+                />
+
+                <TextField 
+                    fullWidth 
+                    label="Password" 
+                    type="password" 
+                    variant="outlined" 
+                    margin="normal" 
+                    InputProps={{ style: { borderRadius: "10px" } }}
+                />
+
+                <Typography variant="body2" style={{ marginTop: "5px", color: "#1976d2", cursor: "pointer" }}>
+                    Forgot Password?
+                </Typography>
+
+                <Button 
+                    fullWidth 
+                    variant="contained" 
+                    color="primary" 
+                    style={{ marginTop: "15px", borderRadius: "10px", padding: "10px" }}>
+                    LOGIN
+                </Button>
+            </Paper>
+        </div>
     );
 }
 
